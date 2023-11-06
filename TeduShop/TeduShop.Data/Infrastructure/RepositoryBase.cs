56be7ca7e0949c6085c578776b3e8aa84b8316a0
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-11-07 05:39:11 +0700

    Triển khai phần Service xử lý business logic cho dự án

--- TeduShop/TeduShop.Data/Infrastructure/RepositoryBase.cs
+++ TeduShop/TeduShop.Data/Infrastructure/RepositoryBase.cs
@@ -48,6 +48,11 @@ namespace TeduShop.Data.Infrastructure
         {
             dbSet.Remove(entity);
         }
+        public virtual void Delete(int id)
+        {
+            var entity = dbSet.Find(id);
+            dbSet.Remove(entity);
+        }
 
         public virtual void DeleteMulti(Expression<Func<T, bool>> where)
         {
