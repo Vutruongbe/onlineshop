10072b8749478f7dab283be031af9a49b3372ff2
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-11-15 15:32:13 +0700

    Test service,  ng-click,  ng-model,  cách gọi sự kiện

--- TeduShop/TeduShop/Scripts/spa/app.js
+++ TeduShop/TeduShop/Scripts/spa/app.js
@@ -1,26 +1,34 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 //B18: $scope, $rootScope, $scope lồng nhau 
+//B19: service, ng-click, ng-model, cách gọi sự kiện
 
 
 var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service('Validator', Validator);
 
-//function studentController($rootScope, $scope) {
-//    $rootScope.message = "Student";
-//    //$scope.message = "Student";
-//};
+schoolController.$inject = ['$scope', 'Validator'];
 
-function schoolController($scope) {
-    $scope.message = "School";
-};
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
 
-function studentController($scope) {
-    //$scope.message = "Student";
-};
-
-function teacherController($scope) {
-    //$scope.message = "Teacher";
-};
+function Validator($window) {
+    return {
+        checkNumber : checkNumber
+        //tên pulic : tên nội bộ
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            //$window.alert('Đây là số chẵn');
+            return 'Đây là số chẵn';
+        }
+        else 
+            //$window.alert('Đây là số lẻ');
+            return 'Đây là số lẻ';
+    }
+}
