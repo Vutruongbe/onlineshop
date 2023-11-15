4d641ca2617fc9e7f1f242ab8832a2b8823d13bc
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-11-15 15:11:12 +0700

    Test $scope $rootScope $scope lồng nhau

--- TeduShop/TeduShop/Controllers/AdminController.cs
+++ TeduShop/TeduShop/Controllers/AdminController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace TeduShop.Web.Controllers
+namespace TeduShop.Controllers
 {
     public class AdminController : Controller
     {
---
+++ TeduShop/TeduShop/Scripts/spa/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+//B18: $scope, $rootScope, $scope lồng nhau 
+
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+//function studentController($rootScope, $scope) {
+//    $rootScope.message = "Student";
+//    //$scope.message = "Student";
+//};
+
+function schoolController($scope) {
+    $scope.message = "School";
+};
+
+function studentController($scope) {
+    //$scope.message = "Student";
+};
+
+function teacherController($scope) {
+    //$scope.message = "Teacher";
+};
