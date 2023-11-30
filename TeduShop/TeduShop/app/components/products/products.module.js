5f3b52918e642e67c2d8a75699efc037ec222325
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-11-30 13:20:59 +0700

    Tạo Form Đăng nhập cho người Quản trị

--- TeduShop/TeduShop/app/components/products/products.module.js
+++ TeduShop/TeduShop/app/components/products/products.module.js
@@ -9,13 +9,16 @@
         $stateProvider.state('products', {
             url: "/products",
             templateUrl: "/app/components/products/productListView.html",
+            parent: 'base',
             controller: "productListController"
         }).state('product_add', {
             url: "/product_add",
+            parent: 'base',
             templateUrl: "/app/components/products/productAddView.html",
             controller: "productAddController"
         }).state('product_edit', {
             url: "/product_edit/:id",
+            parent: 'base',
             templateUrl: "/app/components/products/productEditView.html",
             controller: "productEditController"
         });
