1ef494c0e49ce470ccbaa06adb7f1cbfb24cd45e
Vũ Trường BÉ <vutruongbe25122003@gmail.com>
2023-12-07 04:58:54 +0700

    Sử dụng output cache trong ASP.NET MVC

--- TeduShop/TeduShop/Controllers/HomeController.cs
+++ TeduShop/TeduShop/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace TeduShop.Web.Controllers
             _productService = productService;
         }
 
+        [OutputCache(Duration = 60, Location = System.Web.UI.OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             var slideModel = _commonService.GetSlides();
@@ -42,25 +43,14 @@ namespace TeduShop.Web.Controllers
             return View(homeViewModel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Footer()
         {
             var footerModel = _commonService.GetFooter();
             var footerViewModel = Mapper.Map<Footer, FooterViewModel>(footerModel);
+            //ViewBag.Time = DateTime.Now.ToString("T");
             return PartialView(footerViewModel);
         }
 
@@ -71,6 +61,7 @@ namespace TeduShop.Web.Controllers
         }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Category()
         {
             var model = _productCategoryService.GetAll();
